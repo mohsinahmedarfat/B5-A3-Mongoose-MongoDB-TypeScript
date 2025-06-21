1b34a7ebea5b6f33eadb53f9439fd3b22eb240fe
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-06-21 20:24:33 +0600

    complete: Borrowed Books Summary (Using Aggregation)

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -1,38 +1,61 @@
-import express, { Request, Response } from 'express';
-import { Borrow } from '../models/borrow.model';
+import express, { Request, Response } from "express";
+import { Borrow } from "../models/borrow.model";
 
 export const borrowRouter = express.Router();
 
 borrowRouter.post("/", async (req: Request, res: Response) => {
   const body = req.body;
 
-  const borrowBook = await Borrow.create(body)
+  const borrowBook = await Borrow.create(body);
 
   res.status(201).json({
     success: true,
     message: "Book borrowed successfully",
-    data: borrowBook
+    data: borrowBook,
   });
 });
 
 borrowRouter.get("/", async (req: Request, res: Response) => {
-    // const borrowBooks = await Borrow.find().populate("book")
-    const borrowBooks = await Borrow.aggregate([
-        {$match: {}},
-        {$lookup: {
-            from: "books",
-            localField: "book",
-            foreignField: "_id",
-            as: "book"
-        }},
-        {$unwind: "$book"},
-        {$project: {"book.title": 1, "book.isbn": 1}}
-        
-    ])
+  // const borrowBooks = await Borrow.find().populate("book")
+  const borrowBooks = await Borrow.aggregate([
+    { $match: {} },
+    {
+      $lookup: {
+        from: "books",
+        localField: "book",
+        foreignField: "_id",
+        as: "book",
+      },
+    },
+    { $unwind: "$book" },
+    {
+      $group: {
+        _id: "$book",
+        totalQuantity: { $sum: "$quantity" },
+      },
+    },
+    {
+      $project: {
+        book: {
+          title: "$_id.title",
+          isbn: "$_id.isbn",
+        },
+        // book: {
+        //   _id: "$_id._id",
+        //   title: "$_id.title",
+        //   copies: "$_id.copies",
+        //   available: "$_id.available",
+        //   quantity: "$_id.quantity",
+        // },
+        totalQuantity: 1,
+        _id: 0,
+      },
+    },
+  ]);
 
   res.status(201).json({
     success: true,
     message: "Borrowed books summary retrieved successfully",
-    data: borrowBooks
+    data: borrowBooks,
   });
 });
